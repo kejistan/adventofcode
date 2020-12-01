ab298c9f89f329af2c6f62f46827e9ac2cb51b28
Sean Nicolay <nicolay@flexport.com>
2020-11-30 22:48:39 -0800

    Day 1 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use itertools::Itertools;
+
+fn main() -> io::Result<()> {
+  let reader = BufReader::new(io::stdin());
+
+  let numbers = reader.lines().map(Result::unwrap).map(|line| line.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+  let result = numbers.iter().cartesian_product(numbers.iter()).find(|(&a, &b)| {
+    a + b == 2020
+  });
+  match result {
+    Some((a, b)) => println!("{} * {} = {}", a, b, a * b),
+    None => println!("no result found"),
+  }
+  Ok(())
 }

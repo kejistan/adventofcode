905f1deb5cd07cd5bcf2312c2cee88e998ff09b8
Sean Nicolay <nicolay@flexport.com>
2020-12-02 21:19:23 -0800

    Day 3 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use regex::Regex;
-
-struct Rule {
-  index_one: usize,
-  index_two: usize,
-  character: char,
-}
 
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
 
-  let line_regex = Regex::new(r"(\d+)-(\d+) (.): (.+$)").unwrap();
-  let count = reader.lines().map(Result::unwrap).filter(|line| {
-    let captures = line_regex.captures(&line).unwrap();
-    let rule = Rule {
-      index_one: str::parse::<usize>(&captures[1]).unwrap() - 1,
-      index_two: str::parse::<usize>(&captures[2]).unwrap() - 1,
-      character: captures[3].chars().next().unwrap(),
-    };
+  let (_, count) = reader.lines().map(Result::unwrap).map(|line| {
+    line.chars().map(|c| c != '.').collect::<Vec<bool>>()
+  }).fold((0, 0), |(x, count), line| {
+    let next = x + 3;
+    if line[x % line.len()] {
+      (next, count + 1)
+    } else {
+      (next, count)
+    }
+  });
 
-    let password = &captures[4];
-    rule.check_password(password)
-  }).count();
-
-  println!("{} matched passwords", count);
+  println!("{} trees on slope (1, 3)", count);
   Ok(())
-}
-
-impl Rule {
-  fn check_password(&self, password: &str) -> bool {
-    let characters = password.chars().collect::<Vec<char>>();
-    let index_one_match = characters.get(self.index_one).map(|&character| character == self.character).unwrap_or(false);
-    let index_two_match = characters.get(self.index_two).map(|&character| character == self.character).unwrap_or(false);
-    index_one_match ^ index_two_match
-  }
 }

6dc19b6a3771ba5097b4de37174fee2395f5e70a
Sean Nicolay <nicolay@flexport.com>
2020-12-05 21:16:44 -0800

    Day 6 - part 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -6,16 +6,23 @@ use std::io::BufReader;
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
 
-  let mut answers: HashSet<char> = HashSet::new();
+  let mut answers: HashMap<char, i32> = HashMap::new();
   let mut total = 0;
+  let mut group_size = 0;
   for l in reader.lines() {
     if let Ok(line) = l {
       if line.is_empty() {
-        total += answers.len();
+        total += answers.iter().filter(|(_, &count)| count == group_size).count();
         answers.clear();
+        group_size = 0;
       } else {
+        group_size += 1;
         for c in line.chars() {
-          answers.insert(c);
+          if let Some(count) = answers.get_mut(&c) {
+            *count += 1;
+          } else {
+            answers.insert(c, 1);
+          }
         }
       }
     } else {

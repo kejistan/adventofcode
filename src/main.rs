a3671c4fbdb100341c3d9f42b0b764f3a0d39b0a
Sean Nicolay <nicolay@flexport.com>
2020-12-07 21:24:06 -0800

    Day 8 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,66 @@
-use std::collections::HashMap;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use regex::Regex;
+
+enum Operation {
+  NOP,
+  ACC,
+  JMP,
+}
+
+struct Instruction {
+  op: Operation,
+  operand: isize,
+  visited: bool,
+}
 
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
-  let base_rule_regex = Regex::new(r"^(\w+ \w+) bags contain ").unwrap();
-  let empty_content_regex = Regex::new(r"no other bags.").unwrap();
-  let content_regex = Regex::new(r"(\d+) (\w+ \w+) bags?").unwrap();
 
-  let parsed_rules = reader.lines().map(|l| {
+  let mut instructions = reader.lines().map(|l| {
     let line = l.unwrap();
-    let bag_type: String = base_rule_regex.captures(&line).unwrap().get(1).map_or("", |s| s.as_str()).to_string();
-    let content_str: &str = &line[(bag_type.len() + " bags contain ".len())..];
-    if empty_content_regex.is_match(content_str) {
-      (bag_type, HashMap::default())
-    } else {
-      let mut counts = HashMap::new();
-      for capture in content_regex.captures_iter(content_str) {
-        let count = capture.get(1).unwrap().as_str().parse::<i32>().unwrap();
-        let bag_type = capture.get(2).unwrap().as_str().to_string();
+    let operation = match &line[0..3] {
+      "nop" => Operation::NOP,
+      "acc" => Operation::ACC,
+      "jmp" => Operation::JMP,
+      _ => panic!(),
+    };
+    let negative = &line[4..5] == "-";
+    let mut operand = line[5..].parse::<isize>().unwrap();
 
-        counts.insert(bag_type, count);
-      }
-      (bag_type, counts)
+    if negative {
+      operand *= -1;
     }
-  });
 
-  // Mapping of each bag and the bags it contains
-  let mut rules = HashMap::new();
-  for (bag_type, contents) in parsed_rules {
-    rules.insert(bag_type, contents);
+    Instruction {
+      op: operation,
+      operand: operand,
+      visited: false,
+    }
+  }).collect::<Vec<Instruction>>();
+
+  let mut accumulator = 0;
+  let mut pc = 0;
+
+  loop {
+    let instruction = instructions.get_mut(pc).unwrap();
+    if instruction.visited {
+      break;
+    }
+
+    instruction.visited = true;
+
+    match instruction.op {
+      Operation::NOP => pc += 1,
+      Operation::ACC => {
+        accumulator += instruction.operand;
+        pc += 1;
+      },
+      Operation::JMP => pc = ((pc as isize) + instruction.operand) as usize,
+    }
   }
 
-  let result = recursive_count_children(&rules, "shiny gold");
-
-  println!("shiny gold bag contains {} other bags", result);
+  println!("accumulator: {}", accumulator);
 
   Ok(())
-}
-
-fn recursive_count_children(rules: &HashMap<String, HashMap<String, i32>>, bag: &str) -> i32 {
-  rules.get(bag).unwrap().iter().fold(0, |total, (bag, count)| {
-    total + count * (1 + recursive_count_children(rules, bag))
-  })
 }

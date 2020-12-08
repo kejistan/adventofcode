c01331e0d6488482fe2f7752b4ad9b15bfe20688
Sean Nicolay <nicolay@flexport.com>
2020-12-08 02:16:31 -0800

    optimization, only visit each instruction once

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+#[derive(PartialEq)]
 enum Operation {
   NOP,
   ACC,
@@ -11,6 +12,7 @@ enum Operation {
 struct Instruction {
   op: Operation,
   operand: isize,
+  accumulator: Option<isize>,
   visited: bool,
 }
 
@@ -35,9 +37,17 @@ fn main() -> io::Result<()> {
     Instruction {
       op: operation,
       operand: operand,
+      accumulator: None,
       visited: false,
     }
   }).collect::<Vec<Instruction>>();
+
+  let first_instruction = &mut instructions[0];
+  first_instruction.accumulator = Some(0);
+  first_instruction.visited = true;
+
+  // initialize cached accumulators
+  run_instructions(&mut instructions, 0, true);
 
   let mut target = 0;
   loop {
@@ -46,61 +56,75 @@ fn main() -> io::Result<()> {
     }
 
     toggle_instruction_at(&mut instructions, target);
-    target += 1;
 
-    // reset instruction state
-    for instruction in instructions.iter_mut() {
-      instruction.visited = false;
-    }
-
-    let (success, accumulator) = run_instructions(&mut instructions);
-    if success {
-      println!("accumulator: {}", accumulator);
+    if let Some(result) = run_instructions(&mut instructions, target, false) {
+      println!("result: {}", result);
       break;
     }
 
+    target += 1;
     toggle_instruction_at(&mut instructions, target - 1);
   }
 
   Ok(())
 }
 
-fn run_instructions(instructions: &mut Vec<Instruction>) -> (bool, isize) {
-  let mut pc = 0;
-  let mut accumulator = 0;
+fn run_instructions(instructions: &mut Vec<Instruction>, initial_pc: usize, set_accumulators: bool) -> Option<isize> {
+  // special case for first instruction, we want to ignore the accumulator cache's
+  // presence.
+  let mut pc = initial_pc;
+  let first_instruction = instructions.get(pc).unwrap();
+  if first_instruction.accumulator == None {
+    // this is pc is unreachable code
+    return None;
+  }
+
+  let mut accumulator = first_instruction.accumulator.unwrap();
+  if first_instruction.op == Operation::ACC {
+    accumulator += first_instruction.operand;
+  }
+  pc = calculate_pc(first_instruction, pc);
+
   loop {
     match instructions.get_mut(pc) {
       None => break,
       Some(instruction) => {
-        if instruction.visited {
-          break;
+        if instruction.visited == true {
+          return None;
         }
 
         instruction.visited = true;
+        if set_accumulators {
+          instruction.accumulator = Some(accumulator);
+        }
 
-        match instruction.op {
-          Operation::NOP => pc += 1,
-          Operation::ACC => {
-            accumulator += instruction.operand;
-            pc += 1;
-          },
-          Operation::JMP => {
-            let new_pc = (pc as isize) + instruction.operand;
-            if new_pc < 0 {
-              break;
-            } else {
-              pc = new_pc as usize;
-            }
-          }
+        pc = calculate_pc(instruction, pc);
+        if instruction.op == Operation::ACC {
+          accumulator += instruction.operand;
         }
       },
     }
   }
 
   if pc == instructions.len() {
-    (true, accumulator)
+    Some(accumulator)
   } else {
-    (false, accumulator)
+    None
+  }
+}
+
+fn calculate_pc(instruction: &Instruction, pc: usize) -> usize {
+  match instruction.op {
+    Operation::NOP => pc + 1,
+    Operation::ACC => pc + 1,
+    Operation::JMP => {
+      let new_pc = (pc as isize) + instruction.operand;
+      if new_pc < 0 {
+        panic!("negative pc");
+      } else {
+        new_pc as usize
+      }
+    }
   }
 }
 

b0bbeebfeb7b83c66b041d54d9a59278a7ce9580
Sean Nicolay <nicolay@flexport.com>
2020-12-08 21:26:26 -0800

    Day 9 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,40 @@
+use std::collections::HashSet;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
+use circular_queue::CircularQueue;
 
-#[derive(PartialEq)]
-enum Operation {
-  NOP,
-  ACC,
-  JMP,
-}
-
-struct Instruction {
-  op: Operation,
-  operand: isize,
-  accumulator: Option<isize>,
-  visited: bool,
+struct Entry {
+  number: i32,
+  sum_cache: HashSet<i32>,
 }
 
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
 
-  let mut instructions = reader.lines().map(|l| {
-    let line = l.unwrap();
-    let operation = match &line[0..3] {
-      "nop" => Operation::NOP,
-      "acc" => Operation::ACC,
-      "jmp" => Operation::JMP,
-      _ => panic!(),
-    };
-    let negative = &line[4..5] == "-";
-    let mut operand = line[5..].parse::<isize>().unwrap();
+  let numbers = reader.lines().map(|l| l.unwrap().parse::<i32>().unwrap());
 
-    if negative {
-      operand *= -1;
+  let window_size = 25;
+  let mut window: CircularQueue<Entry> = CircularQueue::with_capacity(window_size);
+  for number in numbers {
+    for entry in window.iter_mut() {
+      entry.sum_cache.insert(entry.number + number);
     }
 
-    Instruction {
-      op: operation,
-      operand: operand,
-      accumulator: None,
-      visited: false,
-    }
-  }).collect::<Vec<Instruction>>();
+    if window.is_full() {
+      let mut result = false;
+      for entry in window.iter() {
+        result = result || entry.sum_cache.contains(&number);
+      }
 
-  let first_instruction = &mut instructions[0];
-  first_instruction.accumulator = Some(0);
-  first_instruction.visited = true;
-
-  // initialize cached accumulators
-  run_instructions(&mut instructions, 0, true);
-
-  let mut target = 0;
-  loop {
-    if target >= instructions.len() {
-      panic!("fix not found!");
+      if !result {
+        println!("invalid entry {}", number);
+        break;
+      }
     }
 
-    toggle_instruction_at(&mut instructions, target);
-
-    if let Some(result) = run_instructions(&mut instructions, target, false) {
-      println!("result: {}", result);
-      break;
-    }
-
-    target += 1;
-    toggle_instruction_at(&mut instructions, target - 1);
+    window.push(Entry { number: number, sum_cache: HashSet::new() });
   }
 
   Ok(())
-}
-
-fn run_instructions(instructions: &mut Vec<Instruction>, initial_pc: usize, set_accumulators: bool) -> Option<isize> {
-  // special case for first instruction, we want to ignore the accumulator cache's
-  // presence.
-  let mut pc = initial_pc;
-  let first_instruction = instructions.get(pc).unwrap();
-  if first_instruction.accumulator == None {
-    // this is pc is unreachable code
-    return None;
-  }
-
-  let mut accumulator = first_instruction.accumulator.unwrap();
-  if first_instruction.op == Operation::ACC {
-    accumulator += first_instruction.operand;
-  }
-  pc = calculate_pc(first_instruction, pc);
-
-  loop {
-    match instructions.get_mut(pc) {
-      None => break,
-      Some(instruction) => {
-        if instruction.visited == true {
-          return None;
-        }
-
-        instruction.visited = true;
-        if set_accumulators {
-          instruction.accumulator = Some(accumulator);
-        }
-
-        pc = calculate_pc(instruction, pc);
-        if instruction.op == Operation::ACC {
-          accumulator += instruction.operand;
-        }
-      },
-    }
-  }
-
-  if pc == instructions.len() {
-    Some(accumulator)
-  } else {
-    None
-  }
-}
-
-fn calculate_pc(instruction: &Instruction, pc: usize) -> usize {
-  match instruction.op {
-    Operation::NOP => pc + 1,
-    Operation::ACC => pc + 1,
-    Operation::JMP => {
-      let new_pc = (pc as isize) + instruction.operand;
-      if new_pc < 0 {
-        panic!("negative pc");
-      } else {
-        new_pc as usize
-      }
-    }
-  }
-}
-
-fn toggle_instruction_at(instructions: &mut Vec<Instruction>, target: usize) {
-  let instruction = match instructions.get_mut(target) {
-    None => panic!("None for target {}", target),
-    Some(i) => i,
-  };
-
-  match instruction.op {
-    Operation::ACC => (),
-    Operation::JMP => {
-      instruction.op = Operation::NOP
-    },
-    Operation::NOP => {
-      instruction.op = Operation::JMP
-    },
-  }
 }

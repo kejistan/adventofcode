8211f7bfcca21a960991f12f77a121350fc7d215
Sean Nicolay <nicolay@flexport.com>
2020-12-09 22:14:35 -0800

    Day 10 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::VecDeque;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -6,27 +5,24 @@ use std::io::BufReader;
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
 
-  let numbers = reader.lines().map(|l| l.unwrap().parse::<i32>().unwrap());
+  let mut adapters = reader.lines().map(|l| l.unwrap().parse::<i32>().unwrap()).collect::<Vec<i32>>();
+  adapters.sort();
 
-  let target = 41682220;
-  let mut window: VecDeque<i32> = VecDeque::new();
-  for number in numbers {
-    window.push_back(number);
-    let mut sum: i32 = window.iter().sum();
-    
-    while sum > target {
-      if let Some(popped) = window.pop_front() {
-        sum -= popped;
-      }
+  let mut threes = 1;
+  let mut ones = 0;
+  let mut prev = 0;
+
+  for adapter in adapters {
+    match adapter - prev {
+      1 => ones += 1,
+      3 => threes += 1,
+      _ => (),
     }
 
-    if sum == target {
-      let min = window.iter().min().unwrap();
-      let max = window.iter().max().unwrap();
-      println!("{} + {} = {}", min, max, min + max);
-      break;
-    }
+    prev = adapter;
   }
+
+  println!("{} * {} = {}", ones, threes, ones * threes);
 
   Ok(())
 }

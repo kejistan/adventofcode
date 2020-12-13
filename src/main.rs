3f4d05f06da008ee782f2be924afbd39c4befb0d
Sean Nicolay <nicolay@flexport.com>
2020-12-13 12:47:14 -0800

    Day 13 - part 1

--- src/main.rs
+++ src/main.rs
@@ -2,63 +2,27 @@ use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-enum Direction {
-  North,
-  East,
-  South,
-  West,
-  Left,
-  Right,
-  Forward,
-}
-
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
 
-  let instructions = reader.lines().map(|l| {
-    let line = l.unwrap();
-    let characters = line.as_str();
-    let direction = match &characters[0..1] {
-      "N" => Direction::North,
-      "E" => Direction::East,
-      "S" => Direction::South,
-      "W" => Direction::West,
-      "L" => Direction::Left,
-      "R" => Direction::Right,
-      "F" => Direction::Forward,
-      _ => panic!(),
-    };
+  let mut lines = reader.lines();
+  let minimum = lines.next().unwrap().unwrap().parse::<i32>().unwrap();
+  let buses = lines.next().unwrap().unwrap().split_terminator(',')
+    .filter(|bus| *bus != "x")
+    .map(|bus| bus.parse::<i32>().unwrap())
+    .collect::<Vec<i32>>();
 
-    let value = characters[1..].parse::<i32>().unwrap();
-
-    (direction, value)
-  });
-
-  let (_, pos) = instructions.fold(((10, 1), (0, 0)), |(waypoint, pos), (direction, value)| {
-    match direction {
-      Direction::North => ((waypoint.0, waypoint.1 + value), pos),
-      Direction::East => ((waypoint.0 + value, waypoint.1), pos),
-      Direction::South => ((waypoint.0, waypoint.1 - value), pos),
-      Direction::West => ((waypoint.0 - value, waypoint.1), pos),
-      Direction::Left => (rotate(waypoint, value), pos),
-      Direction::Right => (rotate(waypoint, -value), pos),
-      Direction::Forward => (waypoint, (pos.0 + value * waypoint.0, pos.1 + value * waypoint.1)),
+  let mut target = minimum;
+  loop {
+    for bus in buses.iter() {
+      if target % bus == 0 {
+        println!("bus {} at {}", bus, target);
+        let wait = target - minimum;
+        println!("wait {}", wait);
+        println!("result: {}", wait * bus);
+        return Ok(());
+      }
     }
-  });
-
-  let distance = pos.0.abs() + pos.1.abs();
-  println!("{}", distance);
-
-  Ok(())
-}
-
-fn rotate(pos: (i32, i32), degrees: i32) -> (i32, i32) {
-  let (x, y) = pos;
-  match degrees.rem_euclid(360) {
-    0 => (x, y),
-    90 => (-y, x),
-    180 => (-x, -y),
-    270 => (y, -x),
-    _ => panic!("{}", degrees),
+    target += 1;
   }
 }

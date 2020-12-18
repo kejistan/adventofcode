0c4073481eaf8a5d5be6d0415c6148d342638602
Sean Nicolay <nicolay@flexport.com>
2020-12-18 00:31:52 -0800

    Day 18 - part 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::boxed::Box;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Token {
   Num(u64),
   Sum,
@@ -13,15 +14,21 @@ enum Token {
 
 use Token::*;
 
+#[derive(Debug)]
+enum Expression {
+  Num(u64),
+  Sum(Box<Expression>, Box<Expression>),
+  Mul(Box<Expression>, Box<Expression>),
+}
+
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
  
   let mut result = 0;
   for l in reader.lines() {
     let tokens = tokenize(&l.unwrap());
-    let (num, _) = parse(&tokens);
-
-    result += num;
+    let expression = parse(&tokens);
+    result += evaluate(expression);
   }
 
   println!("result: {}", result);
@@ -29,55 +36,80 @@ fn main() -> io::Result<()> {
   Ok(())
 }
 
-fn parse<'a>(mut tokens: &'a [Token]) -> (u64, &'a [Token]) {
-  let mut result = 0;
-  let mut operation: Option<Token> = None;
-
-  while !tokens.is_empty() {
-    match tokens[0] {
-      Close => {
-        return (result, &tokens[1..])
-      },
-      Open => {
-        let (num, new_tokens) = parse(&tokens[1..]);
-        if let Some(op) = operation {
-          result = perform(result, &op, num);
-          operation = None;
-        } else {
-          result = num;
-        }
-        tokens = new_tokens;
-      },
-      Sum => {
-        operation = Some(Sum);
-        tokens = &tokens[1..];
-      },
-      Mul => {
-        operation = Some(Mul);
-        tokens = &tokens[1..];
-      },
-      Num(num) => {
-        if let Some(op) = operation {
-          result = perform(result, &op, num);
-          operation = None;
-        } else {
-          result = num;
-        }
-
-        tokens = &tokens[1..];
-      }
-    }
+fn evaluate(expression: Expression) -> u64 {
+  match expression {
+    Expression::Num(num) => num,
+    Expression::Mul(l, r) => evaluate(*l) * evaluate(*r),
+    Expression::Sum(l, r) => evaluate(*l) + evaluate(*r),
   }
-
-  (result, &[])
 }
 
-fn perform(left: u64, operation: &Token, right: u64) -> u64 {
-  match operation {
-    Sum => left + right,
-    Mul => left * right,
-    _ => panic!(),
+fn parse_num<'a>(tokens: &'a [Token]) -> (Expression, &'a [Token]) {
+  match tokens[0] {
+    Num(num) => {
+      (Expression::Num(num), &tokens[1..])
+    },
+    _ => panic!("{:?}", tokens[0]),
   }
+}
+
+fn parse_unit<'a>(tokens: &'a [Token]) -> (Expression, &'a [Token]) {
+  match tokens[0] {
+    Num(_) => parse_num(tokens),
+    Open => parse_paren(tokens),
+    _ => panic!("{:?}", tokens[0]),
+  }
+}
+
+fn parse_paren<'a>(tokens: &'a [Token]) -> (Expression, &'a [Token]) {
+  if tokens[0] != Open {
+    panic!("{:?}", tokens[0]);
+  }
+
+  let (expr, more_tokens) = parse_expression(&tokens[1..]);
+
+  if more_tokens[0] != Close {
+    panic!("{:?}", more_tokens[0]);
+  }
+
+  (expr, &more_tokens[1..])
+}
+
+fn parse_sum<'a>(tokens: &'a [Token]) -> (Expression, &'a [Token]) {
+  let (mut left, mut more_tokens) = parse_unit(tokens);
+
+  while !more_tokens.is_empty() && more_tokens[0] == Sum {
+    let (right, t) = parse_unit(&more_tokens[1..]);
+    left = Expression::Sum(Box::new(left), Box::new(right));
+    more_tokens = t;
+  }
+
+  (left, more_tokens)
+}
+
+fn parse_mul<'a>(tokens: &'a [Token]) -> (Expression, &'a [Token]) {
+  let (mut left, mut more_tokens) = parse_sum(tokens);
+
+  while !more_tokens.is_empty() && more_tokens[0] == Mul {
+    let (right, t) = parse_sum(&more_tokens[1..]);
+    left = Expression::Mul(Box::new(left), Box::new(right));
+    more_tokens = t;
+  }
+
+  (left, more_tokens)
+}
+
+fn parse_expression<'a>(tokens: &'a [Token]) -> (Expression, &'a [Token]) {
+  parse_mul(tokens)
+}
+
+fn parse<'a>(tokens: &'a [Token]) -> Expression {
+  let (expr, more_tokens) = parse_expression(tokens);
+  if !more_tokens.is_empty() {
+    panic!("{:?}", more_tokens);
+  }
+
+  expr
 }
 
 fn tokenize(string: &str) -> Vec<Token> {

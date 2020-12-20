b3a917c9341706d22d118e0a2b50fe97f993ce4d
Sean Nicolay <nicolay@flexport.com>
2020-12-19 21:53:29 -0800

    Day 20 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,156 +1,147 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io;
-use std::io::prelude::*;
 use std::io::BufReader;
 use regex::Regex;
 
-#[derive(Debug)]
-enum Rule {
-  Literal(char),
-  Ref(u32),
-  Or(Vec<Rule>),
-  Seq(Vec<Rule>),
-}
+mod grouped_iterator;
+use grouped_iterator::GroupedIterator;
 
-use Rule::*;
+struct Image {
+  data: Vec<Vec<bool>>,
+  left_data: u32,
+  right_data: u32,
+  top_data: u32,
+  bottom_data: u32,
+}
 
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
-  let mut lines = reader.lines().into_iter();
-  let rule_line_regex = Regex::new(r"^(\d+): (.+)$").unwrap();
-  
-  let mut tokenized_rules = HashMap::new();
-  while let Some(l) = lines.next() {
-    let line = l.unwrap();
-    if line.is_empty() {
-      break;
+  let name_regex = Regex::new(r"Tile (\d+):").unwrap();
+
+  let mut images = HashMap::new();
+  for g in reader.groups() {
+    let group = g.unwrap();
+    let mut lines = group.lines();
+    let label = name_regex.captures(lines.next().unwrap()).unwrap()[1].parse::<u32>().unwrap();
+    let data = lines.map(|l| l.chars().map(|c| {
+      match c {
+        '.' => false,
+        '#' => true,
+        _ => panic!(),
+      }
+    }).collect::<Vec<bool>>()).collect::<Vec<Vec<bool>>>();
+
+    let mut left_data = 0;
+    let mut right_data = 0;
+    for row in data.iter() {
+      left_data = left_data << 1;
+      if row[0] {
+        left_data += 1;
+      }
+      right_data = right_data << 1;
+      if row[row.len() - 1] {
+        right_data += 1;
+      }
     }
+    let mut top_data = 0;
+    for b in data[0].iter() {
+      top_data = top_data << 1;
+      if *b {
+        top_data += 1;
+      }
+    }
+    let mut bottom_data = 0;
+    for b in data[data.len() - 1].iter() {
+      bottom_data = bottom_data << 1;
+      if *b {
+        bottom_data += 1;
+      }
+    }
+    let image = Image {
+      data: data,
+      left_data: left_data,
+      right_data: right_data,
+      top_data: top_data,
+      bottom_data: bottom_data,
+    };
 
-    let captures = rule_line_regex.captures(&line).unwrap();
-    let label = captures[1].parse::<u32>().unwrap();
-    let rule = &captures[2];
+    images.insert(label, image);
+  }
 
-    if rule.chars().nth(0).unwrap() == '"' {
-      tokenized_rules.insert(label, Literal(rule.chars().nth(1).unwrap()));
-    } else if rule.contains('|') {
-      tokenized_rules.insert(label, Or(rule.split('|').map(parse_seq).collect()));
-    } else {
-      tokenized_rules.insert(label, parse_seq(rule));
+  let mut images_by_signatures: HashMap<u32, HashSet<u32>> = HashMap::new();
+
+  for (label, image) in images.iter() {
+    insert_image_by_signatures(&mut images_by_signatures, *label, image);
+  }
+
+  let mut corners = Vec::new();
+  for (label, image) in images {
+    let count = count_unmatched_sides(&images_by_signatures, &image);
+    if count == 2 {
+      corners.push(label);
+    }
+    if count > 2 {
+      panic!();
     }
   }
 
-  tokenized_rules.remove(&0);
-  tokenized_rules.remove(&8);
-  tokenized_rules.remove(&11);
-
-  let mut count = 0;
-  for l in lines {
-    let line = l.unwrap();
-    let result = check_rules(&tokenized_rules, &line);
-
-    if result {
-      count += 1;
-    }
-  }
-
-  println!("{}", count);
+  println!("corners: {:?}", corners);
+  println!("{}", corners.into_iter().fold(1, |result, label| result * label as u64));
 
   Ok(())
 }
 
-fn check_rules(tokenized_rules: &HashMap<u32, Rule>, mut string: &str) -> bool {
-  // rule 0: 8 11
-  // rule 8: 42 | 42 8
-  // rule 11: 42 31 | 42 11 31
-
-  let rule42 = tokenized_rules.get(&42).unwrap();
-  while !string.is_empty() {
-    if let (true, more) = check_rule(tokenized_rules, rule42, string) {
-      string = more;
-      if check_rule_11(tokenized_rules, string) {
-        return true;
-      }
-    } else {
-      break;
-    }
+fn count_unmatched_sides(images_by_signatures: &HashMap<u32, HashSet<u32>>, image: &Image) -> usize {
+  let mut count = 0;
+  if side_is_unmatched(images_by_signatures, image.left_data) {
+    count += 1;
+  }
+  if side_is_unmatched(images_by_signatures, image.right_data) {
+    count += 1;
+  }
+  if side_is_unmatched(images_by_signatures, image.top_data) {
+    count += 1;
+  }
+  if side_is_unmatched(images_by_signatures, image.bottom_data) {
+    count += 1;
   }
 
-  false
+  count
 }
 
-fn check_rule_11(tokenized_rules: &HashMap<u32, Rule>, mut string: &str) -> bool {
-  let rule42 = tokenized_rules.get(&42).unwrap();
-  let rule31 = tokenized_rules.get(&31).unwrap();
-
-  let mut num = 0;
-  while !string.is_empty() {
-    if let (true, more) = check_rule(tokenized_rules, rule42, string) {
-      num += 1;
-      string = more;
-
-      let mut cont = string;
-      let mut i = 0;
-      while i < num && !cont.is_empty() {
-        if let (true, more) = check_rule(tokenized_rules, rule31, cont) {
-          cont = more;
-        } else {
-          break;
-        }
-
-        i += 1;
-      }
-
-      if i == num && cont.is_empty() {
-        return true;
-      }
-    } else {
-      break;
-    }
-  }
-
-  false
+fn side_is_unmatched(images_by_signatures: &HashMap<u32, HashSet<u32>>, signature: u32) -> bool {
+  images_by_signatures.get(&signature).unwrap().len() == 1 && images_by_signatures.get(&flip_signature(signature)).unwrap().len() == 1
 }
 
-fn parse_seq(string: &str) -> Rule {
-  Rule::Seq(string.split(' ').filter(|s| !s.is_empty()).map(str::parse).map(Result::unwrap).map(Ref).collect::<Vec<Rule>>())
+fn insert_image_by_signatures(images_by_signatures: &mut HashMap<u32, HashSet<u32>>, label: u32, image: &Image) {
+  add_image_signature(images_by_signatures, label, image.left_data);
+  add_image_signature(images_by_signatures, label, image.right_data);
+  add_image_signature(images_by_signatures, label, image.top_data);
+  add_image_signature(images_by_signatures, label, image.bottom_data);
+  add_image_signature(images_by_signatures, label, flip_signature(image.left_data));
+  add_image_signature(images_by_signatures, label, flip_signature(image.right_data));
+  add_image_signature(images_by_signatures, label, flip_signature(image.top_data));
+  add_image_signature(images_by_signatures, label, flip_signature(image.bottom_data));
 }
 
-fn check_rule<'a>(tokenized_rules: &HashMap<u32, Rule>, rule: &Rule, string: &'a str) -> (bool, &'a str) {
-  if string.is_empty() {
-    return (false, string);
+fn add_image_signature(images_by_signatures: &mut HashMap<u32, HashSet<u32>>, label: u32, signature: u32) {
+  if let Some(set) = images_by_signatures.get_mut(&signature) {
+    set.insert(label);
+  } else {
+    let mut set = HashSet::new();
+    set.insert(label);
+    images_by_signatures.insert(signature, set);
+  }
+}
+
+fn flip_signature(mut signature: u32) -> u32 {
+  let mut flipped = 0;
+  for _ in 0..10 {
+    flipped = flipped << 1;
+    flipped += signature & 1;
+    signature = signature >> 1;
   }
 
-  match rule {
-    Literal(character) => {
-      let is_match = string.chars().nth(0).unwrap() == *character;
-      (is_match, &string[1..])
-    },
-    Ref(label) => {
-      check_rule(tokenized_rules, tokenized_rules.get(label).unwrap(), string)
-    },
-    Seq(rules) => {
-      let mut is_match = true;
-      let mut more_str = string;
-      for rule in rules.iter() {
-        let (did_match, more) = check_rule(tokenized_rules, rule, more_str);
-        is_match &= did_match;
-        more_str = more;
-        if !is_match {
-          break;
-        }
-      }
-
-      (is_match, more_str)
-    },
-    Or(rules) => {
-      for rule in rules.iter() {
-        let (did_match, more) = check_rule(tokenized_rules, rule, string);
-        if did_match {
-          return (true, more);
-        }
-      }
-
-      (false, string)
-    }
-  }
+  flipped
 }

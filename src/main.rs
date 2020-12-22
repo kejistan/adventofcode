82f6d4cf64014f144a8a6faa973764a0a5b17abf
Sean Nicolay <nicolay@flexport.com>
2020-12-21 21:35:53 -0800

    Day 22 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,48 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use regex::Regex;
-
-struct Food {
-  ingredients: Vec<String>,
-  allergens: Vec<String>,
-}
 
 fn main() -> io::Result<()> {
   let reader = BufReader::new(io::stdin());
-  let allergen_regex = Regex::new(r"(\w+)[,)]").unwrap();
-  let foods = reader.lines().map(|l| {
-    let line = l.unwrap();
-    let parts = line.split("(contains ").collect::<Vec<&str>>();
-    let ingredients = parts[0].split(' ').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect::<Vec<String>>();
-    let allergens = allergen_regex.captures_iter(parts[1]).map(|cap| cap[1].to_string()).collect::<Vec<String>>();
+  let mut lines = reader.lines();
+  lines.next(); // Player 1:
 
-    Food {
-      ingredients,
-      allergens,
+  let mut player1 = VecDeque::new();
+  let mut player2 = VecDeque::new();
+  while let Some(Ok(line)) = lines.next() {
+    if line.is_empty() {
+      break;
     }
-  });
+    player1.push_front(line.parse::<u32>().unwrap());
+  }
 
-  let mut allergen_possibilities: HashMap<String, HashSet<String>> = HashMap::new();
-  for food in foods {
-    let possibilities = food.ingredients.iter().map(String::clone).collect::<HashSet<String>>();
-    for allergen in food.allergens {
-      if let Some(set) = allergen_possibilities.get_mut(allergen.as_str()) {
-        *set = set.intersection(&possibilities).map(|s| s.clone()).collect::<HashSet<String>>();
-      } else {
-        allergen_possibilities.insert(allergen, possibilities.clone());
-      }
+  lines.next(); // Player 2:
+  while let Some(Ok(line)) = lines.next() {
+    player2.push_front(line.parse::<u32>().unwrap());
+  }
+
+  while !player1.is_empty() && !player2.is_empty() {
+    let card1 = player1.pop_back().unwrap();
+    let card2 = player2.pop_back().unwrap();
+
+    if card1 > card2 {
+      player1.push_front(card1);
+      player1.push_front(card2);
+    } else {
+      player2.push_front(card2);
+      player2.push_front(card1);
     }
   }
 
-  let mut known_allergens = Vec::with_capacity(allergen_possibilities.len());
-  while !allergen_possibilities.is_empty() {
-    let (a, _) = allergen_possibilities.iter().find(|(_, set)| set.len() == 1).unwrap();
-    let allergen = a.clone();
-    let (allergen, set) = allergen_possibilities.remove_entry(&allergen).unwrap();
-    let ingredient = set.into_iter().next().unwrap();
-    for (_, set) in allergen_possibilities.iter_mut() {
-      set.remove(&ingredient);
-    }
-    known_allergens.push((allergen, ingredient));
-  }
+  let winning_deck = if !player1.is_empty() {
+    player1
+  } else {
+    player2
+  };
 
-  known_allergens.sort_by(|(a, _), (b, _)| a.cmp(b));
-  let sorted_ingredients = known_allergens.into_iter().map(|(_, ingredient)| ingredient).collect::<Vec<String>>();
-  println!("{}", sorted_ingredients.join(","));
+  let score = winning_deck.into_iter().enumerate().fold(0, |score, (i, card)| score + (i + 1) as u64 * card as u64);
+  println!("{}", score);
 
   Ok(())
 }

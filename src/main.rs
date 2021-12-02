c05109147b1fb097ab8646091815142fa02f26b4
Sean Nicolay <nicolay@flexport.com>
2021-12-01 21:21:06 -0800

    Day 2, Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::collections::VecDeque;
+use regex::Regex;
+
+enum Command {
+  Forward(u32),
+  Up(u32),
+  Down(u32),
+}
 
 fn main() -> io::Result<()> {
   let input = BufReader::new(io::stdin());
-  let mut depths = input.lines().map(|l| l.unwrap().parse::<u32>().unwrap());
+  let re = Regex::new(r"^(?P<direction>[[:alpha:]]+) (?P<count>\d+)$").unwrap();
 
-  let mut window: VecDeque<u32> = VecDeque::with_capacity(3);
-  for _ in 0..3 {
-    window.push_back(depths.next().unwrap());
-  }
+  let commands = input.lines().map(|l| {
+    let line = l.unwrap();
+    let cap = re.captures(&line).unwrap();
+    let count = cap.name("count").unwrap().as_str().parse::<u32>().unwrap();
 
-  let mut count = 0;
-  for depth in depths {
-    let prev: u32 = window.iter().sum();
-    window.pop_front();
-    window.push_back(depth);
-    let curr: u32 = window.iter().sum();
+    match cap.name("direction").unwrap().as_str() {
+      "forward" => Command::Forward(count),
+      "up" => Command::Up(count),
+      "down" => Command::Down(count),
+      _ => panic!(),
+    }
+  });
 
-    if curr > prev {
-      count += 1;
+  let mut pos = (0, 0);
+
+  for command in commands {
+    match command {
+      Command::Forward(count) => pos.0 += count,
+      Command::Down(count) => pos.1 += count,
+      Command::Up(count) => pos.1 -= count,
     }
   }
 
-  println!("{}", count);
+  println!("{:?}", pos);
+  println!("{}", pos.0 * pos.1);
 
   Ok(())
 }

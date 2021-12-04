4fe83192b5e0e835b326ef9d7c9f27aadea38044
Sean Nicolay <nicolay@flexport.com>
2021-12-03 21:56:55 -0800

    Day 4, Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,89 @@
 use std::io;
-use std::io::prelude::*;
 use std::io::BufReader;
+
+mod grouped_iterator;
+use grouped_iterator::GroupedIterator;
 
 fn main() -> io::Result<()> {
   let input = BufReader::new(io::stdin());
+  let mut groups = input.groups();
+  let numbers = groups.next().unwrap()?.split(',').map(|num| num.parse::<u32>().unwrap()).collect::<Vec<u32>>();
 
-  let mut width = 0;
-  let mut numbers = vec![];
+  let mut boards = Vec::with_capacity(5);
 
-  for l in input.lines() {
-    let mut number = 0;
-    let line = l.unwrap();
-    if width == 0 {
-      width = line.len();
+  for input in groups {
+    let mut board = Vec::with_capacity(5);
+    for line in input.unwrap().lines() {
+      board.push(line.split_ascii_whitespace().map(|num| (num.parse::<u32>().unwrap(), false)).collect::<Vec<(u32, bool)>>());
     }
 
-    for bit in line.chars() {
-      number <<= 1;
-
-      match bit {
-        '1' => {
-          number += 1;
-        }
-        _ => {}
-      }
-    }
-
-    numbers.push(number);
+    boards.push(board);
   }
 
-  let (gamma, epsilon) = find_rates(&numbers, width);
+  let mut score = 0;
+  for number in numbers {
+    if let Some(winner) = mark(&mut boards, number) {
+      score = number * score_board(winner);
+      break;
+    }
+  }
 
-  println!("{:0width$b}", gamma, width = width);
-  println!("{:0width$b}", epsilon, width = width);
-
-  let oxygen = find_rating(numbers.clone(), width, Type::Gamma);
-  let co2 = find_rating(numbers.clone(), width, Type::Epsilon);
-  println!("oxygen {}", oxygen);
-  println!("co2 {}", co2);
-  println!("{}", oxygen * co2);
+  println!("{}", score);
 
   Ok(())
 }
 
-enum Type {
-  Gamma,
-  Epsilon,
-}
-
-fn find_rating(mut numbers: Vec<u32>, width: usize, rate_type: Type) -> u32 {
-  let mut bit = 1;
-  while width >= bit && numbers.len() > 1 {
-    let (gamma, epsilon) = find_rates(&numbers, width);
-    let rate = match rate_type {
-      Type::Gamma => gamma,
-      Type::Epsilon => epsilon,
-    };
-
-    let mask = 1 << width - bit;
-    numbers = numbers.into_iter().filter(|num| num & mask == rate & mask).collect::<Vec<u32>>();
-
-    bit += 1;
+fn mark(boards: &mut Vec<Vec<Vec<(u32, bool)>>>, number: u32) -> Option<&Vec<Vec<(u32, bool)>>> {
+  for board in boards {
+    let board = mark_board(board, number);
+    if board.is_some() {
+      return board;
+    }
   }
 
-  numbers[0]
+  None
 }
 
-fn find_rates(numbers: &Vec<u32>, width: usize) -> (u32, u32) {
-  let mut one_bits = vec![];
-  for num in numbers {
-    for i in 0..width {
-      if one_bits.len() == i {
-        one_bits.push(0);
-      }
-
-      let mask = 1 << i;
-      if num & mask != 0 {
-        one_bits[i] += 1;
+fn mark_board(board: &mut Vec<Vec<(u32, bool)>>, number: u32) -> Option<&Vec<Vec<(u32, bool)>>> {
+  'mark_loop: for row in board.iter_mut() {
+    for (num, marked) in row.iter_mut() {
+      if *num == number {
+        *marked = true;
+        break 'mark_loop;
       }
     }
   }
 
-  let mut gamma = 0;
-  let mut epsilon = 0;
+  if check_board(board) {
+    return Some(&*board);
+  }
 
-  one_bits.reverse();
+  None
+}
 
-  for count in one_bits {
-    gamma <<= 1;
-    epsilon <<= 1;
-
-    if 2 * count >= numbers.len() {
-      gamma += 1;
-    } else {
-      epsilon += 1;
+fn check_board(board: &Vec<Vec<(u32, bool)>>) -> bool {
+  // check rows
+  for row in board {
+    if row.iter().all(|(_, marked)| *marked) {
+      return true;
     }
   }
-  
-  (gamma, epsilon)
+
+  // check columns
+  for x in 0..5 {
+    if (0..5).all(|y| board[y][x].1) {
+      return true;
+    }
+  }
+
+  false
+}
+
+fn score_board(board: &Vec<Vec<(u32, bool)>>) -> u32 {
+  let mut sum = 0;
+  for row in board {
+    sum += row.iter().filter(|(_, marked)| !marked).map(|(val, _)| val).sum::<u32>();
+  }
+
+  sum
 }

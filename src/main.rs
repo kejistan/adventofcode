01899b2ff9e45eff1c094a8553596a774ef8188d
Sean Nicolay <nicolay@flexport.com>
2021-12-04 21:41:48 -0800

    Day 5, Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,54 @@
 use std::io;
 use std::io::BufReader;
-
-mod grouped_iterator;
-use grouped_iterator::GroupedIterator;
+use std::io::BufRead;
+use std::collections::HashMap;
+use std::cmp::max;
+use std::cmp::min;
+use regex::Regex;
+use core::iter::Iterator;
 
 fn main() -> io::Result<()> {
+  let re = Regex::new(r"(?P<x1>\d+),(?P<y1>\d+) -> (?P<x2>\d+),(?P<y2>\d+)").unwrap();
   let input = BufReader::new(io::stdin());
-  let mut groups = input.groups();
-  let numbers = groups.next().unwrap()?.split(',').map(|num| num.parse::<u32>().unwrap()).collect::<Vec<u32>>();
 
-  let mut boards = Vec::with_capacity(5);
+  let mut map = HashMap::new();
 
-  for input in groups {
-    let mut board = Vec::with_capacity(5);
-    for line in input.unwrap().lines() {
-      board.push(line.split_ascii_whitespace().map(|num| (num.parse::<u32>().unwrap(), false)).collect::<Vec<(u32, bool)>>());
-    }
+  let iter = input.lines().map(|l| {
+    let line = l.unwrap();
+    let cap = re.captures(&line).unwrap();
+    let x1 = cap["x1"].parse::<u32>().unwrap();
+    let y1 = cap["y1"].parse::<u32>().unwrap();
+    let x2 = cap["x2"].parse::<u32>().unwrap();
+    let y2 = cap["y2"].parse::<u32>().unwrap();
 
-    boards.push(board);
+    ((x1, y1), (x2, y2))
+  }).filter(|(start, end)| {
+    start.0 == end.0 || start.1 == end.1
+  });
+
+  for line in iter {
+    draw_line(&mut map, line);
   }
 
-  let mut score = 0;
-  for number in numbers {
-    let is_final_board = boards.len() == 1;
-    mark(&mut boards, number);
+  let count = map.values().filter(|count| **count > 1).count();
 
-    if is_final_board && check_board(&boards[0]) {
-      score = number * score_board(&boards[0]);
-      break;
-    } else {
-      boards = boards.into_iter().filter(|board| !check_board(board)).collect();
-    }
-  }
-
-  println!("{}", score);
+  println!("{}", count);
 
   Ok(())
 }
 
-fn mark(boards: &mut Vec<Vec<Vec<(u32, bool)>>>, number: u32) {
-  for board in boards {
-    mark_board(board, number);
-  }
-}
-
-fn mark_board(board: &mut Vec<Vec<(u32, bool)>>, number: u32) {
-  'mark_loop: for row in board.iter_mut() {
-    for (num, marked) in row.iter_mut() {
-      if *num == number {
-        *marked = true;
-        break 'mark_loop;
-      }
+fn draw_line(map: &mut HashMap<(u32, u32), u32>, (start, end): ((u32, u32), (u32, u32))) {
+  if start.0 == end.0 {
+    for y in min(start.1, end.1)..=max(start.1, end.1) {
+      let count = map.entry((start.0, y)).or_insert(0);
+      *count += 1;
     }
-  }
-}
-
-fn check_board(board: &Vec<Vec<(u32, bool)>>) -> bool {
-  // check rows
-  for row in board {
-    if row.iter().all(|(_, marked)| *marked) {
-      return true;
+  } else if start.1 == end.1 {
+    for x in min(start.0, end.0)..=max(start.0, end.0) {
+      let count = map.entry((x, start.1)).or_insert(0);
+      *count += 1;
     }
+  } else {
+    panic!();
   }
-
-  // check columns
-  for x in 0..5 {
-    if (0..5).all(|y| board[y][x].1) {
-      return true;
-    }
-  }
-
-  false
-}
-
-fn score_board(board: &Vec<Vec<(u32, bool)>>) -> u32 {
-  let mut sum = 0;
-  for row in board {
-    sum += row.iter().filter(|(_, marked)| !marked).map(|(val, _)| val).sum::<u32>();
-  }
-
-  sum
 }

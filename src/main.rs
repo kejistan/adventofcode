e041c7c4f3ec8b3aa194568cd57f33661939aaa2
Sean Nicolay <nicolay@flexport.com>
2021-12-06 21:16:43 -0800

    Day 7, Part 1

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,29 @@ use std::io;
 use std::io::BufReader;
 use std::io::BufRead;
 use regex::Regex;
-use std::collections::VecDeque;
 
 fn main() -> io::Result<()> {
   let re = Regex::new(r"\d+").unwrap();
   let input = BufReader::new(io::stdin());
 
-  let mut fish_counts = VecDeque::with_capacity(9);
-  for _ in 0..9 {
-    fish_counts.push_back(0);
-  }
-  for age in re.captures_iter(&input.lines().next().unwrap().unwrap()).map(|caps| caps.get(0).unwrap().as_str().parse::<usize>().unwrap()) {
-    fish_counts[age] += 1;
+  let positions = re.captures_iter(&input.lines().next().unwrap().unwrap()).map(|caps| caps.get(0).unwrap().as_str().parse::<u32>().unwrap()).collect::<Vec<u32>>();
+  let max = positions.iter().max().unwrap();
+
+  let mut min = None;
+  for i in 0..*max {
+    let cost = total_fuel_cost(&positions, i);
+    match min {
+      None => min = Some(cost),
+      Some(best_so_far) if best_so_far > cost => min = Some(cost),
+      _ => {},
+    }
   }
 
-  for _ in 0..256 {
-    advance(&mut fish_counts);
-  }
-
-  println!("{}", fish_counts.iter().sum::<u64>());
+  println!("{}", min.unwrap());
 
   Ok(())
 }
 
-fn advance(fish_counts: &mut VecDeque<u64>) {
-  let fish = fish_counts.pop_front().unwrap();
-  fish_counts[6] += fish;
-  fish_counts.push_back(fish);
+fn total_fuel_cost(positions: &Vec<u32>, target: u32) -> u32 {
+  positions.iter().fold(0, |cost, pos| cost + (*pos as i32 - target as i32).abs() as u32)
 }

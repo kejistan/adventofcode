bd614201018fdca5eb1af273643c8fb86ba6a921
Sean Nicolay <sean@loop.com>
2022-12-01 21:23:21 -0800

    Day 2, Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,57 @@
-use std::{io, mem};
-use std::io::BufReader;
+use core::panic;
+use std::{io};
+use std::io::{BufReader, BufRead};
 
-mod grouped_iterator;
-use grouped_iterator::GroupedIterator;
+#[derive(PartialEq, Clone, Copy)]
+enum Shape {
+  Rock = 1,
+  Paper,
+  Scissors,
+}
+
+struct Round {
+  opponent: Shape,
+  me: Shape,
+}
 
 fn main() -> io::Result<()> {
   let input = BufReader::new(io::stdin());
 
-  let top_three: u32 = input.groups().map(|lines| -> u32 {
-    lines.unwrap().lines().map(|line| -> u32 {
-      line.parse::<u32>().unwrap()
-    }).sum()
-  }).max_n(3).iter().sum();
+  let rounds = input.lines().map(|line| {
+    let line_string = line.unwrap();
+    let mut round_shapes = line_string.split_ascii_whitespace().take(2).map(|str| match str {
+      "A" => Shape::Rock,
+      "B" => Shape::Paper,
+      "C" => Shape::Scissors,
+      "X" => Shape::Rock,
+      "Y" => Shape::Paper,
+      "Z" => Shape::Scissors,
+      _ => panic!(),
+    });
 
-  println!("{}", top_three);
+    Round {
+      opponent: round_shapes.next().unwrap(),
+      me: round_shapes.next().unwrap(),
+    }
+  });
+
+  let score: u32 = rounds.map(|round| round.score()).sum();
+
+  println!("{}", score);
 
   Ok(())
 }
 
-trait MaxN<T> {
-  fn max_n(&mut self, num: usize) -> Vec<T>;
-}
+impl Round {
+  fn score(&self) -> u32 {
+    let outcome = match self.me {
+      x if x == self.opponent => 3,
+      Shape::Rock if self.opponent == Shape::Paper => 0,
+      Shape::Paper if self.opponent == Shape::Scissors => 0,
+      Shape::Scissors if self.opponent == Shape::Rock => 0,
+      _ => 6,
+    };
 
-impl<T: Ord, U: Iterator<Item = T>> MaxN<T> for U {
-  fn max_n(&mut self, num: usize) -> Vec<T> {
-    self.fold(vec![], |mut max, mut value| {
-      if max.len() < num {
-        max.push(value);
-        max.sort();
-        max.reverse();
-        return max;
-      }
-
-      for max_value in max.iter_mut() {
-        if *max_value < value {
-          mem::swap(&mut *max_value, &mut value);
-        }
-      }
-
-      max
-    })
+    outcome + (self.me as u32)
   }
 }

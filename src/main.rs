6520b74482fe258e6380990e133e5d6facffa4f2
Sean Nicolay <sean@loop.com>
2022-12-02 21:23:58 -0800

    Day 3, Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,31 @@
-use core::panic;
+use std::collections::HashSet;
 use std::{io};
 use std::io::{BufReader, BufRead};
-
-#[derive(PartialEq, Clone, Copy)]
-enum Shape {
-  Rock = 1,
-  Paper,
-  Scissors,
-}
-
-enum Outcome {
-  Win,
-  Draw,
-  Lose,
-}
-
-struct Round {
-  opponent: Shape,
-  outcome: Outcome,
-}
 
 fn main() -> io::Result<()> {
   let input = BufReader::new(io::stdin());
 
-  let rounds = input.lines().map(|line| {
+  let score: u32 = input.lines().map(|line| {
     let line_string = line.unwrap();
-    let mut letters = line_string.split_ascii_whitespace().take(2);
-  
-    Round {
-      opponent: match letters.next().unwrap() {
-        "A" => Shape::Rock,
-        "B" => Shape::Paper,
-        "C" => Shape::Scissors,
-        _ => panic!(),
-      },
-      outcome: match letters.next().unwrap() {
-        "X" => Outcome::Lose,
-        "Y" => Outcome::Draw,
-        "Z" => Outcome::Win,
-        _ => panic!(),
-      },
-    }
-  });
+    let line_bytes = line_string.as_bytes();
+    let line_len = line_bytes.len();
 
-  let score: u32 = rounds.map(|round| round.score()).sum();
+    let first_compartment = line_bytes[0..line_len / 2].iter().map(byte_to_priority).collect::<HashSet<u8>>();
+    let second_compartment = line_bytes[line_len / 2..line_len].iter().map(byte_to_priority).collect::<HashSet<u8>>();
+
+    let overlap = first_compartment.intersection(&second_compartment);
+    *overlap.into_iter().next().unwrap() as u32
+  }).sum();
 
   println!("{}", score);
 
   Ok(())
 }
 
-impl Round {
-  fn score(&self) -> u32 {
-    match self.outcome {
-      Outcome::Draw => 3 + self.opponent as u32,
-      Outcome::Lose => match self.opponent {
-        Shape::Rock => Shape::Scissors as u32,
-        Shape::Paper => Shape::Rock as u32,
-        Shape::Scissors => Shape::Paper as u32,
-      }
-      Outcome::Win => 6 + match self.opponent {
-        Shape::Rock => Shape::Paper as u32,
-        Shape::Paper => Shape::Scissors as u32,
-        Shape::Scissors => Shape::Rock as u32,
-      }
-    }
+fn byte_to_priority(&byte: &u8) -> u8 {
+  if byte >= 'a' as u8 {
+    byte - 'a' as u8 + 1
+  } else {
+    byte - 'A' as u8 + 27
   }
 }

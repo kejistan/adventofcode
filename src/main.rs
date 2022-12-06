6f19487490c5459bccfd033ca95b2c8b3cd1a22b
Sean Nicolay <sean@loop.com>
2022-12-05 21:13:16 -0800

    Day 6, Part 2

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> io::Result<()> {
   let input = BufReader::new(io::stdin());
   let line = input.lines().next().unwrap()?;
 
-  let (result, _) = line.as_bytes().windows(4).enumerate().find(|(i, window)| {
+  let (result, _) = line.as_bytes().windows(14).enumerate().find(|(i, window)| {
     for i in 0..window.len() {
       let element = window[i];
       if window[i+1..].contains(&element) {
@@ -16,7 +16,7 @@ fn main() -> io::Result<()> {
     true
   }).unwrap();
 
-  println!("{}", result + 4);
+  println!("{}", result + 14);
 
   Ok(())
 }

7e36beb0edf1f18d034a1e210ae5d4e58a1481cd
Sean Nicolay <sean@loop.com>
2022-12-12 22:19:44 -0800

    Day 13, Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,64 @@
 use std::collections::VecDeque;
 use std::{io};
-use std::io::{BufReader};
-
-use crate::grouped_iterator::GroupedIterator;
+use std::io::{BufReader, BufRead};
 
 mod grouped_iterator;
+
+#[derive(Eq, PartialEq)]
+struct Packet {
+  tokens: Vec<Token>,
+}
 
 fn main() -> io::Result<()> {
   let input = BufReader::new(io::stdin());
 
-  let results = input.groups().map(|result| {
-    let pair_str = result.unwrap();
-    let mut lines = pair_str.lines();
-    let left_packet = tokenize(lines.next().unwrap());
-    let right_packet = tokenize(lines.next().unwrap());
-
-    packets_are_in_order(left_packet, right_packet)
-  });
-
-  let result: usize = results.enumerate().map(|(i, result)| {
-    if result {
-      i + 1
-    } else {
-      0
+  let packets = input.lines().filter_map(|result| {
+    let line = result.unwrap();
+    if line == "" {
+      return None
     }
-  }).sum();
 
+    Some(Packet { tokens: tokenize(&line) })
+  }).collect::<Vec<Packet>>();
+
+  let mut divider_one_idx = 1;
+  let mut divider_two_idx = 2;
+  let divider_one = Packet { tokens: tokenize("[[2]]") };
+  let divider_two = Packet { tokens: tokenize("[[6]]") };
+  for packet in packets {
+    if packet < divider_one {
+      divider_one_idx += 1;
+    }
+    if packet < divider_two {
+      divider_two_idx += 1;
+    }
+  }
+
+  let result = divider_one_idx * divider_two_idx;
   println!("{}", result);
 
   Ok(())
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 enum Token {
   Number(u8),
   ListStart,
   ListEnd,
 }
 
-fn tokenize(string: &str) -> VecDeque<Token> {
-  let mut tokens = VecDeque::new();
+fn tokenize(string: &str) -> Vec<Token> {
+  let mut tokens = Vec::new();
   let mut token_start = 0;
   for (token_end, current_char) in string.char_indices() {
     match current_char {
       ',' | '[' | ']' => {
         if token_start != token_end {
-          tokens.push_back(Token::Number(string[token_start..token_end].parse::<u8>().unwrap()));
+          tokens.push(Token::Number(string[token_start..token_end].parse::<u8>().unwrap()));
         }
         match current_char {
-          '[' => tokens.push_back(Token::ListStart),
-          ']' => tokens.push_back(Token::ListEnd),
+          '[' => tokens.push(Token::ListStart),
+          ']' => tokens.push(Token::ListEnd),
           _ => (),
         }
         token_start = token_end + 1;
@@ -61,47 +70,58 @@ fn tokenize(string: &str) -> VecDeque<Token> {
   tokens
 }
 
-fn packets_are_in_order(mut left_vec: VecDeque<Token>, mut right_vec: VecDeque<Token>) -> bool {
-  let mut left = left_vec.pop_front();
-  let mut right = right_vec.pop_front();
+impl PartialOrd for Packet {
+  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    let mut left_queue = VecDeque::from_iter(self.tokens.iter());
+    let mut right_queue = VecDeque::from_iter(other.tokens.iter());
 
-  while left.is_some() && right.is_some() {
-    if left == right {
-      left = left_vec.pop_front();
-      right = right_vec.pop_front();
-      continue;
-    }
-
-    if right == Some(Token::ListEnd) {
-      return false;
-    }
-    if left == Some(Token::ListEnd) {
-      return true;
-    }
-
-    if right == Some(Token::ListStart) {
-      left_vec.push_front(Token::ListEnd);
-      left_vec.push_front(left.unwrap());
-      left = Some(Token::ListStart);
-      continue;
-    }
-    if left == Some(Token::ListStart) {
-      right_vec.push_front(Token::ListEnd);
-      right_vec.push_front(right.unwrap());
-      right = Some(Token::ListStart);
-      continue;
-    }
-
-    if let Token::Number(l) = left.unwrap() {
-      if let Token::Number(r) = right.unwrap() {
-        return l < r;
+    let mut left = left_queue.pop_front();
+    let mut right = right_queue.pop_front();
+    while left.is_some() && right.is_some() {
+      if left == right {
+        left = left_queue.pop_front();
+        right = right_queue.pop_front();
+        continue;
+      }
+  
+      if right == Some(&Token::ListEnd) {
+        return Some(std::cmp::Ordering::Greater);
+      }
+      if left == Some(&Token::ListEnd) {
+        return Some(std::cmp::Ordering::Less);
+      }
+  
+      if right == Some(&Token::ListStart) {
+        left_queue.push_front(&Token::ListEnd);
+        left_queue.push_front(left.unwrap());
+        left = Some(&Token::ListStart);
+        continue;
+      }
+      if left == Some(&Token::ListStart) {
+        right_queue.push_front(&Token::ListEnd);
+        right_queue.push_front(right.unwrap());
+        right = Some(&Token::ListStart);
+        continue;
+      }
+  
+      if let Token::Number(l) = left.unwrap() {
+        if let Token::Number(r) = right.unwrap() {
+          return Some(l.cmp(r));
+        } else {
+          panic!();
+        }
       } else {
         panic!();
       }
-    } else {
-      panic!();
     }
-  }
+  
+    if left == right {
+      return Some(std::cmp::Ordering::Equal);
+    }
+    if left.is_some() {
+      return Some(std::cmp::Ordering::Greater);
+    }
 
-  !left.is_some()
+    Some(std::cmp::Ordering::Less)
+  }
 }

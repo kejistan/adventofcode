e4f42f41afc0cc9eb224bb5830b4e9c749b0f5f6
Sean Nicolay <sean@loop.com>
2022-12-17 13:22:53 -0800

    Day 17, Part 2

--- src/coordinate.rs
+++ src/coordinate.rs
@@ -2,58 +2,58 @@ use std::ops::{AddAssign, Sub, Add};
 use std::fmt::Debug;
 
 #[derive(Clone, Copy, PartialEq, Hash, Eq)]
-pub struct Coordinate {
-  pub x: i32,
-  pub y: i32,
+pub struct Coordinate<T> {
+  pub x: T,
+  pub y: T,
 }
 
-impl Coordinate {
-  pub fn new(x: i32, y: i32) -> Coordinate {
+impl<T> Coordinate<T> {
+  pub fn new(x: T, y: T) -> Coordinate<T> {
     Coordinate { x, y }
   }
 }
 
-impl Debug for Coordinate {
+impl<T: std::fmt::Display> Debug for Coordinate<T> {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     f.write_fmt(format_args!("({},{})", self.x, self.y))
   }
 }
 
-impl Add for Coordinate {
+impl<T: Add<Output = T>> Add for Coordinate<T> {
   type Output = Self;
 
-  fn add(self, other: Self) -> Coordinate {
+  fn add(self, other: Self) -> Coordinate<T> {
     Coordinate::new(self.x + other.x, self.y + other.y)
   }
 }
 
-impl Add for &Coordinate {
-  type Output = Coordinate;
+impl<T: Add<Output = T> + Copy> Add for &Coordinate<T> {
+  type Output = Coordinate<T>;
 
-  fn add(self, other: Self) -> Coordinate {
+  fn add(self, other: Self) -> Coordinate<T> {
     Coordinate::new(self.x + other.x, self.y + other.y)
   }
 }
 
-impl AddAssign for Coordinate {
+impl<T: AddAssign> AddAssign for Coordinate<T> {
   fn add_assign(&mut self, other: Self) {
     self.x += other.x;
     self.y += other.y;
   }
 }
 
-impl Sub for &Coordinate {
-  type Output = Coordinate;
+impl<T: Sub<Output = T> + Copy> Sub for &Coordinate<T> {
+  type Output = Coordinate<T>;
 
-  fn sub(self, other: Self) -> Coordinate {
+  fn sub(self, other: Self) -> Coordinate<T> {
     Coordinate::new(self.x - other.x, self.y - other.y)
   }
 }
 
-impl Sub for Coordinate {
+impl<T: Sub<Output = T>> Sub for Coordinate<T> {
   type Output = Self;
 
-  fn sub(self, other: Self) -> Coordinate {
+  fn sub(self, other: Self) -> Coordinate<T> {
     Coordinate::new(self.x - other.x, self.y - other.y)
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::cmp::max;
 use std::{io};
 use std::io::{BufReader, Read};
 
@@ -6,18 +6,28 @@ use coordinate::Coordinate;
 
 mod coordinate;
 
+type Coord = Coordinate<i64>;
+type Row = [bool; 7];
+
 enum Direction {
   Left,
   Right,
 }
 
 struct Shape {
-  coordinates: Vec<Coordinate>,
+  coordinates: Vec<Coord>,
 }
 
 struct Tunnel {
-  height: i32,
-  occupied: HashSet<Coordinate>,
+  column_heights: [i64; 7],
+  floor: i64,
+  occupied: Vec<Row>,
+}
+
+struct HistoryEntry {
+  occupied: Vec<Row>,
+  floor: i64,
+  rock_count: u64,
 }
 
 fn main() -> io::Result<()> {
@@ -68,29 +78,105 @@ fn main() -> io::Result<()> {
   let mut directions = all_directions.cycle();
   let mut shapes = all_shapes.cycle();
 
-  for _ in 0..2022 {
-    let rock = shapes.next().unwrap();
-    let mut coordinate = tunnel.start_coordinate();
+  let mut history: Vec<Vec<HistoryEntry>> = Vec::with_capacity(all_directions.len());
+  history.resize_with(all_directions.len(), || Vec::new());
 
-    loop {
-      coordinate = handle_jet(&rock, coordinate, &mut directions, &tunnel);
-      if let Some(coord) = fall(&rock, coordinate, &mut tunnel) {
-        coordinate = coord;
-      } else {
-        break;
+  let mut rock_count: u64 = 0;
+  let rock_limit = 1000000000000u64;
+
+  let mut maybe_repeat = None;
+  println!("Finding pattern");
+  'find_pattern: while rock_count < rock_limit {
+    add_rock(&mut shapes, &mut directions, &mut tunnel);
+    rock_count += 1;
+
+    if shapes.idx == 0 {
+      let this_entry = HistoryEntry::new(&tunnel, rock_count);
+      let history_entries = &mut history[directions.idx];
+      for history_entry in history_entries.iter() {
+        if *history_entry == this_entry {
+          let height_difference = this_entry.floor - history_entry.floor;
+          let rock_difference = this_entry.rock_count - history_entry.rock_count;
+          maybe_repeat = Some((rock_difference, height_difference));
+          break 'find_pattern;
+        }
       }
+
+      history_entries.push(this_entry);
+    }
+
+    if rock_count % (1000000000000u64 / 1000) == 0 {
+      let pct = rock_count / (1000000000000u64 / 1000);
+      println!("{}.{}%", pct / 10, pct % 10);
     }
   }
 
+  if rock_count == rock_limit {
+    println!("No repeats found before finishing simulation");
+    println!("{}", tunnel.height());
 
-  let result = tunnel.height;
+    return Ok(());
+  }
 
-  println!("{}", result);
+  let (repeat_rocks, repeat_height) = maybe_repeat.unwrap();
+  println!("Found pattern after {} rocks", rock_count);
+  println!("Pattern repeats every {} rocks and adds {} height", repeat_rocks, repeat_height);
+
+  let skippable_repeats = (rock_limit - rock_count) / repeat_rocks;
+  let rocks_skipped = skippable_repeats * repeat_rocks;
+  let height_skipped = skippable_repeats as i64 * repeat_height;
+  println!("Skipping ahead {} rocks (+{} height)", rocks_skipped, height_skipped);
+
+  tunnel.floor += height_skipped;
+  for col in tunnel.column_heights.iter_mut() {
+    *col += height_skipped;
+  }
+  rock_count += rocks_skipped;
+
+  println!("Simulating remaining rocks");
+  while rock_count < rock_limit {
+    add_rock(&mut shapes, &mut directions, &mut tunnel);
+    rock_count += 1;
+
+    if rock_count % (1000000000000u64 / 1000) == 0 {
+      let pct = rock_count / (1000000000000u64 / 1000);
+      println!("{}.{}%", pct / 10, pct % 10);
+    }
+  }
+
+  println!("{}", tunnel.height());
 
   Ok(())
 }
 
-fn handle_jet(rock: &Shape, mut coordinate: Coordinate, directions: &mut dyn Iterator<Item = &Direction>, tunnel: &Tunnel) -> Coordinate {
+impl HistoryEntry {
+  fn new(tunnel: &Tunnel, rock_count: u64) -> HistoryEntry {
+    let occupied = tunnel.occupied.clone();
+    HistoryEntry { occupied, floor: tunnel.floor, rock_count }
+  }
+}
+
+impl PartialEq for HistoryEntry {
+  fn eq(&self, other: &Self) -> bool {
+    self.occupied.eq(&other.occupied)
+  }
+}
+
+fn add_rock(shapes: &mut dyn Iterator<Item = &Shape>, directions: &mut dyn Iterator<Item = &Direction>, tunnel: &mut Tunnel) {
+  let mut coordinate = tunnel.start_coordinate();
+  let rock = shapes.next().unwrap();
+
+  loop {
+    coordinate = handle_jet(&rock, coordinate, directions, tunnel);
+    if let Some(coord) = fall(&rock, coordinate, tunnel) {
+      coordinate = coord;
+    } else {
+      break;
+    }
+  }
+}
+
+fn handle_jet(rock: &Shape, mut coordinate: Coord, directions: &mut dyn Iterator<Item = &Direction>, tunnel: &Tunnel) -> Coord {
   let direction = directions.next().unwrap();
   match direction {
     Direction::Left => coordinate.x -= 1,
@@ -107,7 +193,7 @@ fn handle_jet(rock: &Shape, mut coordinate: Coordinate, directions: &mut dyn Ite
   coordinate
 }
 
-fn fall(rock: &Shape, mut coordinate: Coordinate, tunnel: &mut Tunnel) -> Option<Coordinate> {
+fn fall(rock: &Shape, mut coordinate: Coord, tunnel: &mut Tunnel) -> Option<Coord> {
   coordinate.y -= 1;
 
   if tunnel.can_place_at(&coordinate, rock) {
@@ -150,25 +236,74 @@ impl<'a, T> Iterator for Cycler<'a, T> {
 
 impl Tunnel {
   fn new() -> Tunnel {
-    Tunnel { height: 0, occupied: HashSet::new() }
+    Tunnel { column_heights: [0; 7], floor: 0, occupied: Vec::new() }
   }
 
-  fn start_coordinate(&self) -> Coordinate {
-    Coordinate::new(2, self.height + 3)
+  fn start_coordinate(&self) -> Coord {
+    Coordinate::new(2, self.height() + 3)
   }
 
-  fn can_place_at(&self, coordinate: &Coordinate, shape: &Shape) -> bool {
-    !shape.coordinates.iter()
-      .map(|offset| coordinate + offset)
-      .any(|coordinate| self.occupied.contains(&coordinate) || coordinate.y < 0 || coordinate.x < 0 || coordinate.x >= 7)
-  }
-
-  fn settle_at(&mut self, coordinate: &Coordinate, shape: &Shape) {
-    for coordinate in shape.coordinates.iter().map(|offset| coordinate + offset) {
-      if self.height < coordinate.y + 1 {
-        self.height = coordinate.y + 1;
-      }
-      self.occupied.insert(coordinate);
+  fn can_place_at(&self, position: &Coord, shape: &Shape) -> bool {
+    let floor = self.floor();
+    if position.y < floor {
+      return false;
     }
+
+    !shape.coordinates.iter()
+      .map(|offset| position + offset)
+      .any(|coordinate| coordinate.x < 0 || coordinate.x >= self.column_heights.len() as i64 || self.is_occupied(&coordinate))
+  }
+
+  fn settle_at(&mut self, position: &Coord, shape: &Shape) {
+    let mut new_floor = None;
+    for coordinate in shape.coordinates.iter().map(|offset| position + offset) {
+      let height = coordinate.y + 1;
+      self.column_heights[coordinate.x as usize] = max(self.column_heights[coordinate.x as usize], height);
+
+      self.mark_occupied(&coordinate);
+
+      let is_new_floor = !(0..self.column_heights.len()).map(|x| Coordinate::new(x as i64, coordinate.y)).any(|coord| !self.is_occupied(&coord));
+      if is_new_floor {
+        new_floor = max(Some(coordinate.y), new_floor);
+      }
+    }
+
+    if let Some(floor) = new_floor {
+      self.set_floor(floor);
+    }
+  }
+
+  fn set_floor(&mut self, floor: i64) {
+    let y: usize = floor as usize - self.floor() as usize;
+    self.occupied.copy_within(y.., 0);
+    self.occupied.truncate(self.occupied.len() - y);
+    self.floor = floor;
+  }
+
+  fn mark_occupied(&mut self, position: &Coord) {
+    let y: usize = position.y as usize - self.floor() as usize;
+    let x: usize = position.x as usize;
+    if y >= self.occupied.len() {
+      self.occupied.resize(y + 1, [false; 7]);
+    }
+    self.occupied[y][x] = true;
+  }
+
+  fn is_occupied(&self, position: &Coord) -> bool {
+    let y: usize = position.y as usize - self.floor() as usize;
+    let x: usize = position.x as usize;
+    if y < self.occupied.len() {
+      self.occupied[y][x]
+    } else {
+      false
+    }
+  }
+
+  fn floor(&self) -> i64 {
+    self.floor
+  }
+
+  fn height(&self) -> i64 {
+    *self.column_heights.iter().max().unwrap()
   }
 }
